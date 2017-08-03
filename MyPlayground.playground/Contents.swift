2a9ef8debabbaa223730d57f2b43630d6ddf01de
Ashish Patel <ashpat86@gmail.com>
2017-08-03 13:28:09 -0400

    first commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let myNewValue = 40 + 2
+
+print("My new value is \(myNewValue).")
---
+++ MyPlayground2.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let age = 33
+let outputstring  = "Someone you know is \(age) years old"
+print (outputstring,age);
