b8db628e21fc6f90a3eab5b255bb5278f9e20e79
Ashish Patel <ashpat86@gmail.com>
2017-08-10 20:47:10 -0400

    updated playground files

---
+++ Playgrounds/Playing With Constants.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let myString = "My name is Dorian Gray"
+
+//myString = "my name is Elliot"
+
+let myAge = 30
+
+//myAge = 31
+
+let iAmPrettyForever = true
+//iAmPrettyForever = false
