37a6dc41352694cec3ea230331027914b1dac581
Ashish Patel <ashpat86@gmail.com>
2017-08-16 11:46:47 -0400

    Updated projects
    These are updated projects

--- MyPlayground.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-//: Playground - noun: a place where people can play
-
-import UIKit
-
-var str = "Hello, playground"
-
-let myNewValue = 40 + 2
-
-print("My new value is \(myNewValue).")
--- MyPlayground2.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-//: Playground - noun: a place where people can play
-
-import UIKit
-
-var str = "Hello, playground"
-
-let age = 33
-let outputstring  = "Someone you know is \(age) years old"
-print (outputstring,age);
